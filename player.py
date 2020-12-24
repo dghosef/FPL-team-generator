89cdc060fbd18478b2019285d4659bb7d5588a10
Joseph Tan <jdtan638@gmail.com>
2020-12-24 01:43:26 -0800

    added player object and module that can extract data from fplreview.com html

---
+++ player.py
@@ -0,0 +1,20 @@
+class Player():
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    def __hash__(self):
+        return hash((self.name, self.position))
+
+    def __eq__(self, other):
+        return (self.name, self.position) == \
+            (other.name, other.position)
+
+    def __ne__(self, other):
+        return not(self == other)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
